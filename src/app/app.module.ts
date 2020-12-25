3491400bee103c6c033b3d0d2c34f7cc271434f5
Tibor Vlasics <tibor.vlasics@gmail.com>
2020-12-25 15:47:07 +0100

    Delete unused modules

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,23 +1,17 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-import { MatCardModule } from '@angular/material/card';
 import { HttpClientJsonpModule, HttpClientModule } from '@angular/common/http';
 import { MatButtonModule } from '@angular/material/button';
 import { MatToolbarModule } from '@angular/material/toolbar';
-import { MatDialogModule } from '@angular/material/dialog';
 import { LayoutModule } from '@angular/cdk/layout';
-import { MatFormFieldModule } from '@angular/material/form-field';
-import { MatInputModule } from '@angular/material/input';
 import {MatProgressSpinnerModule} from '@angular/material/progress-spinner';
 import {MatIconModule} from '@angular/material/icon';
 import {MatSidenavModule} from '@angular/material/sidenav';
-
+import { GoogleMapsModule } from '@angular/google-maps';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { ItemListComponent } from './item-list/item-list.component';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { ReactiveFormsModule } from '@angular/forms';
-import { GoogleMapsModule } from '@angular/google-maps';
 import { GoogleMapComponent } from './google-map/google-map.component';
 import { ObservationDetailsComponent } from './observation-details/observation-details.component';
 
@@ -32,16 +26,11 @@ import { ObservationDetailsComponent } from './observation-details/observation-d
     BrowserModule,
     AppRoutingModule,
     BrowserAnimationsModule,
-    MatCardModule,
     HttpClientModule,
     HttpClientJsonpModule,
     MatButtonModule,
     MatToolbarModule,
-    MatDialogModule,
     LayoutModule,
-    MatFormFieldModule,
-    MatInputModule,
-    ReactiveFormsModule,
     MatProgressSpinnerModule,
     MatIconModule,
     GoogleMapsModule,
